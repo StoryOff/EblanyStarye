2db0ef1ec78ad174cdcf315146b76fbffbb254d1
StoryOff <sotic.roma@gmail.com>
2023-05-21 16:28:18 +0800

    fix

--- EblanyStarye/Program.cs
+++ EblanyStarye/Program.cs
@@ -5,11 +5,11 @@ int FailCount = 0;
 double SuccessChance;
 
 //коробка 1 полностью верная, 2 наполовину верная, 3 полностю неверная. Их индексы 0/1/2
-List<(int, int)> Balls = new List<(int, int)>
+List<List<int>> Balls = new List<List<int>>
 {
-    (1, 1),
-    (1, 0),
-    (0, 0)
+  new List<int> { 1, 1 },
+  new List<int> { 1, 0 },
+  new List<int> { 0, 0 },
 };
 
 //запускаем бесконечный цикл выбора
@@ -20,16 +20,21 @@ void ChooseBox()
 {
     //инициализация переменных, тут не важно
     Random rnd = new Random();
-    int randNumber;
+    int randBoxNumber;
+    int randBallNumber;
+
 
     // запуск бесконечного цикла
-    while(true)
+    while (true)
     {
-        //выбираем рандом число от 0 до 2 (ошибки тут нет, в скобках 3 должно быть)
-        randNumber = rnd.Next(3);
+        //выбираем рандом число от 0 до 2 (ошибки тут нет, в скобках 3 должно быть), обозначающее выбор коробки
+        randBoxNumber = rnd.Next(3);
 
-        //если первый шар верен, добавляем плюс к раундам
-        if (Balls[randNumber].Item1 == 1)
+        //выбираем рандом число от 0 до 1 обозначающее выбор шара в коробке.
+        randBallNumber = rnd.Next(2);
+
+        //если вытащенный шар верен, добавляем плюс к раундам
+        if (Balls[randBoxNumber][randBallNumber] == 1)
         {
             RoundCount++;
         }
@@ -37,7 +42,7 @@ void ChooseBox()
         else continue;
 
         //если выбралась коробка с 2мя верными шарами, то добавляем +1 к победе
-        if (Balls[randNumber].Item1 == 1 && Balls[randNumber].Item2 == 1)
+        if (Balls[randBoxNumber][0] == 1 && Balls[randBoxNumber][1] == 1)
         {
             SuccessCount++;
         }
